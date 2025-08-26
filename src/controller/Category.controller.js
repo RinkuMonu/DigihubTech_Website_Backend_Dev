baf6ad891e3e125857fee7e99e0bbe1cbf12c3d0
niranjan <niranjan@7unique.in>
2025-08-26 09:50:59 +0530

    as

--- src/controller/Category.controller.js
+++ src/controller/Category.controller.js
@@ -9,7 +9,7 @@ export const createCategory = async (req, res) => {
     if (req.file) {
       imageArray = [req.file.path];
     } else if (req.files) {
-      imageArray = req.files.map(file => file.path);
+      imageArray = req.files.map((file) => file.path);
     }
 
     if (!name) {
@@ -29,7 +29,7 @@ export const createCategory = async (req, res) => {
         name,
         description,
         image: imageArray[0],
-        subcategory: subcategory.toLowerCase(),
+        subcategory: subcategory?.toLowerCase(),
       });
       await category.save();
     }
@@ -49,6 +49,115 @@ export const createCategory = async (req, res) => {
       message: "Failed to create category.",
       error: error.message,
     });
+  }
+};
+
+export const createMainCategory = async (req, res) => {
+  const { subcategory } = req.body;
+  
+
+  // if (!subcategory) {
+  //   return res.status(400).json({ message: "Category name is required." });
+  // }
+
+  try {
+    // Check if category already exists
+    const existingCategory = await ProductCategory.findOne({
+      subcategory: subcategory,
+    });
+    if (existingCategory) {
+      return res.status(409).json({ message: "Category already exists." });
+    }
+
+    // Create new category document
+    const newCategory = new ProductCategory({ subcategory: subcategory });
+    await newCategory.save();
+
+    return res.status(201).json({
+      message: "Category created successfully.",
+      category: newCategory,
+    });
+  } catch (error) {
+    // Mongoose validation error
+    if (error.name === "ValidationError") {
+      return res.status(400).json({ message: error.message });
+    }
+    console.error("Error creating category:", error);
+    return res
+      .status(500)
+      .json({ message: "Server error while creating category." });
+  }
+};
+
+export const getMainCategory = async (_, res) => {
+  try {
+    const categories = await ProductCategory.aggregate([
+      {
+        $group: {
+          _id: "$subcategory",
+          id: { $first: "$_id" },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          subcategory: "$_id",
+          id: 1,
+        },
+      },
+    ]);
+
+    res.status(200).json(categories);
+  } catch (error) {
+    console.error("Error fetching subcategories with ids:", error);
+    res
+      .status(500)
+      .json({ message: "Server error fetching subcategories with ids" });
+  }
+};
+
+export const updateMainCategory = async (req, res) => {
+  const { id, subcategory } = req.body;
+  console.log(id);
+
+  if (!id) {
+    return res.status(400).json({ message: "Category ID is required." });
+  }
+
+  if (!subcategory) {
+    return res.status(400).json({ message: "Category name is required." });
+  }
+
+  try {
+    // Check if another category already has this subcategory
+    const existingCategory = await ProductCategory.findOne({
+      subcategory,
+      _id: { $ne: id }, // exclude the current category from duplicate check
+    });
+
+    if (existingCategory) {
+      return res.status(409).json({ message: "Category already exists." });
+    }
+
+    const updatedCategory = await ProductCategory.findByIdAndUpdate(
+      id,
+      { subcategory },
+      { new: true }
+    );
+
+    if (!updatedCategory) {
+      return res.status(404).json({ message: "Category not found." });
+    }
+
+    return res.status(200).json({
+      message: "Category updated successfully.",
+      category: updatedCategory,
+    });
+  } catch (error) {
+    console.error("Error updating category:", error);
+    return res
+      .status(500)
+      .json({ message: "Server error while updating category." });
   }
 };
 
--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,21 +1,29 @@
-import express from 'express';
+import express from "express";
 import {
-    createCategory,
-    getCategories,
-    getCategoryById,
-    updateCategory,
-    deleteCategory,
-} from '../controller/Category.controller.js';
-import { isAdmin } from '../middleware/isAdmin.js';
-import upload from '../middleware/multerConfig.js';
+  createCategory,
+  getCategories,
+  getCategoryById,
+  updateCategory,
+  deleteCategory,
+  createMainCategory,
+  getMainCategory,
+  updateMainCategory,
+} from "../controller/Category.controller.js";
+import { isAdmin } from "../middleware/isAdmin.js";
+import upload from "../middleware/multerConfig.js";
 
 const router = express.Router();
 
-router.post('/',upload.array('image', 5), isAdmin, createCategory);
-router.get('/', getCategories);
-router.get('/:id', isAdmin, getCategoryById);
-router.put('/:id', isAdmin, updateCategory);
+router.post("/", upload.array("image", 5), isAdmin, createCategory);
+
+router.post("/createMainCategory", createMainCategory);
+router.get("/getMainCategory", getMainCategory);
+router.post("/updateMainCategory", updateMainCategory);
+
+router.get("/", getCategories);
+router.get("/:id", isAdmin, getCategoryById);
+router.put("/:id", isAdmin, updateCategory);
 // Route to delete a category by ID
-router.delete('/:id', isAdmin, deleteCategory);
+router.delete("/:id", isAdmin, deleteCategory);
 
 export default router;
